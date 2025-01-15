6ee731d98b90d496c1c9416c7fd006e067f75d87
laote <ihagiu1212@gmail.com>
2025-01-15 15:24:31 +0200

    adding some fonts and colors that will be used later on for the user interface and buttons

--- aplicatie_cutura_generala.py
+++ aplicatie_cutura_generala.py
@@ -1,7 +1,14 @@
 import pygame  # if you don't have pygame installed use the command pip install pygame
 import sys
 
+# Fonts and color part
+font_title = pygame.font.Font(None, 70)
+font_button = pygame.font.Font(None, 45)
 white = (255, 255, 255)
+black = (0, 0, 0)
+blue = (0, 102, 204)
+grey = (200, 200, 200)
+dark_grey = (150, 150, 150)
 
 # Main window of the app
 pygame.init()
@@ -13,6 +20,14 @@ screen_height = screen_info.current_h
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("General Knowledge Application")
+
+
+
+
+
+
+
+
 
 
 def main():
