ef920384889aeca757ef55860e8aa8f117b2003c
laote <ihagiu1212@gmail.com>
2025-01-15 15:02:39 +0200

    main window of the app was created now is time to focus on what user need to see when he run the app, the welcome msg, and the 3 buttons of the app

--- aplicatie_cutura_generala.py
+++ aplicatie_cutura_generala.py
@@ -1,5 +1,33 @@
-# Imports part
-import pygame
-# if you dont have pygame installed use the command pip install pygame
+import pygame  # if you don't have pygame installed use the command pip install pygame
 import sys
 
+white = (255, 255, 255)
+
+# Main window of the app
+pygame.init()
+
+# Automatically detect the screen resolution of the user's display
+screen_info = pygame.display.Info()
+screen_width = screen_info.current_w
+screen_height = screen_info.current_h
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("General Knowledge Application")
+
+
+def main():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill(white)
+        pygame.display.flip()
+
+    pygame.quit()
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main()
