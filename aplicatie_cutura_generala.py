11d163e496cdac4847113aba03dd9ed83e13e78c
laote <ihagiu1212@gmail.com>
2025-01-16 15:14:15 +0200

    what i did today i created the buttons for the app based on mouse position, next i need to add a drop down menu for the first button here will be with 3 difficulties

--- aplicatie_cutura_generala.py
+++ aplicatie_cutura_generala.py
@@ -1,17 +1,21 @@
 import pygame  # if you don't have pygame installed use the command pip install pygame
 import sys
 
+
+# Main window of the app
+pygame.init()
+
+
 # Fonts and color part
-font_title = pygame.font.Font(None, 70)
-font_button = pygame.font.Font(None, 45)
+font_title = pygame.font.Font(None, 72)
+font_button = pygame.font.Font(None, 48)
 white = (255, 255, 255)
 black = (0, 0, 0)
 blue = (0, 102, 204)
 grey = (200, 200, 200)
 dark_grey = (150, 150, 150)
 
-# Main window of the app
-pygame.init()
+
 
 # Automatically detect the screen resolution of the user's display
 screen_info = pygame.display.Info()
@@ -25,7 +29,36 @@ pygame.display.set_caption("General Knowledge Application")
 
 
 
+def draw_main_screen(mouse_pos):
+    screen.fill(white)
 
+    # Title
+    title_text = font_title.render("Welcome!!", True, blue)
+    screen.blit(title_text, (screen_width // 2 - title_text.get_width() // 2, screen_height // 4))
+
+
+    # Buttons
+
+    button_width, button_height = 300, 80
+    button_x = screen_width // 2 - button_width // 2
+    button_y_start = screen_height // 2
+    button_gap = 20
+
+    buttons = ["General Quiz", "Guess the flag", "To be added"]
+
+    for i, button_text in enumerate(buttons):
+        button_y = button_y_start + i * (button_height + button_gap)
+        # Mouse position over the buttons
+        if button_x < mouse_pos[0] < button_x + button_width and button_y < mouse_pos[1] < button_y + button_height:
+            button_color = dark_grey
+        else:
+            button_color = grey
+
+        pygame.draw.rect(screen, button_color, (button_x, button_y, button_width, button_height))
+        pygame.draw.rect(screen, black, (button_x, button_y, button_width, button_height), 2)
+        button_text_rendered = font_button.render(button_text, True, black)
+        screen.blit(button_text_rendered, (button_x + button_width // 2 - button_text_rendered.get_width() // 2, 
+                                           button_y + button_height // 2 - button_text_rendered.get_height() // 2))
 
 
 
@@ -33,11 +66,12 @@ pygame.display.set_caption("General Knowledge Application")
 def main():
     running = True
     while running:
+        mouse_pos = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
-        screen.fill(white)
+        
+        draw_main_screen(mouse_pos)
         pygame.display.flip()
 
     pygame.quit()
