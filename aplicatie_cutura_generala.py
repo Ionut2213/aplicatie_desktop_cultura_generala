f94612165c198b54531cd29a5b7a0670c3b77059
laote <ihagiu1212@gmail.com>
2025-01-21 14:20:39 +0200

    since i start to use a new file for each button from the main screen i did some changes moved some cod in dedicated file,now the general quiz file handle the difficulties now when you click on change difficulty button you can pick one of the difficulty.Now is time to make the new game button functional for now will only open some random questions, after this i will implement the difficulty

--- aplicatie_cutura_generala.py
+++ aplicatie_cutura_generala.py
@@ -1,11 +1,9 @@
-import pygame  # if you don't have pygame installed use the command pip install pygame
+import pygame
 import sys
 from general_quiz import general_quiz_app
 
 # Main window of the app
 pygame.init()
-
-
 
 # Automatically detect the screen resolution of the user's display
 screen_info = pygame.display.Info()
@@ -14,8 +12,6 @@ screen_height = screen_info.current_h
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("General Knowledge Application")
-
-
 
 # Fonts and color part
 font_title = pygame.font.Font(None, 72)
@@ -26,22 +22,13 @@ blue = (0, 102, 204)
 grey = (200, 200, 200)
 dark_grey = (150, 150, 150)
 
-dropdown_open = False
-selected_difficulty = None
-show_difficulty_menu = False
-
-
-
-
 def draw_main_screen(mouse_pos):
     screen.fill(white)
     # Title
     title_text = font_title.render("Welcome!!", True, blue)
     screen.blit(title_text, (screen_width // 2 - title_text.get_width() // 2, screen_height // 4))
 
-
     # Buttons
-
     button_width, button_height = 300, 80
     button_x = screen_width // 2 - button_width // 2
     button_y_start = screen_height // 2
@@ -62,40 +49,8 @@ def draw_main_screen(mouse_pos):
         button_text_rendered = font_button.render(button_text, True, black)
         screen.blit(button_text_rendered, (button_x + button_width // 2 - button_text_rendered.get_width() // 2, 
                                            button_y + button_height // 2 - button_text_rendered.get_height() // 2))
-    if selected_difficulty:
-        difficulty_text = font_button.render(f"Selected difficulty: {selected_difficulty}", True, blue)
-        screen.blit(difficulty_text, (screen_width // 2 - difficulty_text.get_width() // 2, screen_height // 1.2))
-
-
-
-
-
-
-def draw_dropdown_menu(mouse_pos):
-    dropdown_widht, dropdown_height = 300, 60
-    dropdown_x = screen_width // 2 - dropdown_widht // 2
-    dropdown_y_start = screen_height // 2 + 100
-    options = ["Easy", "Medium", "Hard"]
-
-    for i, option in enumerate(options):
-        dropdown_y = dropdown_y_start + i * dropdown_height
-        if dropdown_x < mouse_pos[0] < dropdown_x + dropdown_widht and dropdown_y < mouse_pos[1] < dropdown_y + dropdown_height:
-            option_color = dark_grey
-        else:
-            option_color = grey
-
-        pygame.draw.rect(screen, option_color, (dropdown_x, dropdown_y, dropdown_widht, dropdown_height))
-        pygame.draw.rect(screen, black, (dropdown_x, dropdown_y, dropdown_widht, dropdown_height), 2)
-        option_text = font_button.render(option, True, black)
-        screen.blit(option_text, (dropdown_x + dropdown_widht // 2 - option_text.get_width() // 2, dropdown_y + dropdown_height // 2 - option_text.get_height() // 2))
-
-    if selected_difficulty:
-        difficulty_text = font_button.render(f"Selected difficulty: {selected_difficulty}", True, blue)
-        screen.blit(difficulty_text, (screen_width // 2 - difficulty_text.get_width() // 2, screen_height // 1.2))
-
 
 def main():
-    global dropdown_open, selected_difficulty, show_difficulty_menu
     running = True
     while running:
         mouse_pos = pygame.mouse.get_pos()
@@ -114,43 +69,22 @@ def main():
 
                 general_quiz_button_y = button_y_start
                 if button_x < mouse_pos[0] < button_x + button_width and general_quiz_button_y < mouse_pos[1] < general_quiz_button_y + button_height:
-                    show_difficulty_menu = not show_difficulty_menu
                     print("Button 'General Quiz' was pressed")
                     general_quiz_app()
 
-                
-                guess_the_flag_button_y = button_y_start
+                guess_the_flag_button_y = button_y_start + button_height + button_gap
                 if button_x < mouse_pos[0] < button_x + button_width and guess_the_flag_button_y < mouse_pos[1] < guess_the_flag_button_y + button_height:
                     print("Button 'Guess the flag' was pressed")
 
-                to_be_added_button_y = button_y_start
+                to_be_added_button_y = guess_the_flag_button_y + button_height + button_gap
                 if button_x < mouse_pos[0] < button_x + button_width and to_be_added_button_y < mouse_pos[1] < to_be_added_button_y + button_height:
-                    print("Button 'to be added ' was pressed")
-                
-                if show_difficulty_menu:
-                    dropdown_widht, dropdown_height = 300, 60
-                    dropdown_x = screen_width // 2 - dropdown_widht // 2
-                    dropdown_y_start = screen_height // 2 + 100
-                    options = ["Easy", "Medium", "Hard"]
+                    print("Button 'to be added' was pressed")
 
-                    for i, option in enumerate(options):
-                        dropdown_y = dropdown_y_start + i * dropdown_height
-                        if dropdown_x < mouse_pos[0] < dropdown_x + dropdown_widht and dropdown_y < mouse_pos[1] < dropdown_y + dropdown_height:
-                            selected_difficulty = option
-                            show_difficulty_menu = False
-                            print(f"The selected difficulty : {selected_difficulty}")
-
-
-        if show_difficulty_menu:
-            draw_dropdown_menu(mouse_pos)
-        else:
-            draw_main_screen(mouse_pos)
-
+        draw_main_screen(mouse_pos)
         pygame.display.flip()
 
     pygame.quit()
-    sys.exit()      
-
+    sys.exit()
 
 if __name__ == "__main__":
     main()
--- general_quiz.py
+++ general_quiz.py
@@ -1,7 +1,6 @@
 import pygame
 import sys
 
-
 def general_quiz_app():
     pygame.init()
 
@@ -12,12 +11,34 @@ def general_quiz_app():
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("General Quiz")
 
-
     font_button = pygame.font.Font(None, 48)
     white = (255, 255, 255)
     black = (0, 0, 0)
     grey = (200, 200, 200)
     dark_grey = (150, 150, 150)
+
+    selected_difficulty = None
+    show_difficulty_menu = False
+
+    def draw_difficulty_menu(mouse_pos):
+        nonlocal selected_difficulty
+
+        dropdown_width, dropdown_height = 300, 60
+        dropdown_x = screen_width // 2 - dropdown_width // 2
+        dropdown_y_start = screen_height // 2 + 100
+        options = ["Easy", "Medium", "Hard"]
+
+        for i, option in enumerate(options):
+            dropdown_y = dropdown_y_start + i * dropdown_height
+            if dropdown_x < mouse_pos[0] < dropdown_x + dropdown_width and dropdown_y < mouse_pos[1] < dropdown_y + dropdown_height:
+                option_color = dark_grey
+            else:
+                option_color = grey
+
+            pygame.draw.rect(screen, option_color, (dropdown_x, dropdown_y, dropdown_width, dropdown_height))
+            pygame.draw.rect(screen, black, (dropdown_x, dropdown_y, dropdown_width, dropdown_height), 2)
+            option_text = font_button.render(option, True, black)
+            screen.blit(option_text, (dropdown_x + dropdown_width // 2 - option_text.get_width() // 2, dropdown_y + dropdown_height // 2 - option_text.get_height() // 2))
 
     running = True
     while running:
@@ -30,27 +51,40 @@ def general_quiz_app():
                 if event.key == pygame.K_ESCAPE:
                     running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                button_widht, button_height = 300, 80
-                button_x = screen_width // 2 - button_widht // 2
-                button_y_start = screen_height // 2
+                button_width, button_height = 300, 80
+                button_x = screen_width // 2 - button_width // 2
+                button_y_start = screen_height // 2 - 100
                 button_gap = 20
 
                 new_game_button_y = button_y_start
-                if button_x < mouse_pos[0] < button_x and new_game_button_y < mouse_pos[1] < new_game_button_y + button_height:
+                if button_x < mouse_pos[0] < button_x + button_width and new_game_button_y < mouse_pos[1] < new_game_button_y + button_height:
                     print("New Game Button was pressed")
 
                 change_difficulty_button_y = button_y_start + button_height + button_gap
-                if button_x < mouse_pos[0] < button_x and change_difficulty_button_y < mouse_pos[1] < change_difficulty_button_y + button_height:
-                    print("Change Button difficulty was pressed")
+                if button_x < mouse_pos[0] < button_x + button_width and change_difficulty_button_y < mouse_pos[1] < change_difficulty_button_y + button_height:
+                    show_difficulty_menu = not show_difficulty_menu
 
                 back_to_menu_button_y = change_difficulty_button_y + button_height + button_gap
-                if button_x < mouse_pos[0] < button_x + button_widht and back_to_menu_button_y < mouse_pos[1] < back_to_menu_button_y + button_height:
+                if button_x < mouse_pos[0] < button_x + button_width and back_to_menu_button_y < mouse_pos[1] < back_to_menu_button_y + button_height:
                     running = False
-        
+
+                if show_difficulty_menu:
+                    dropdown_width, dropdown_height = 300, 60
+                    dropdown_x = screen_width // 2 - dropdown_width // 2
+                    dropdown_y_start = screen_height // 2 + 100
+                    options = ["Easy", "Medium", "Hard"]
+
+                    for i, option in enumerate(options):
+                        dropdown_y = dropdown_y_start + i * dropdown_height
+                        if dropdown_x < mouse_pos[0] < dropdown_x + dropdown_width and dropdown_y < mouse_pos[1] < dropdown_y + dropdown_height:
+                            selected_difficulty = option
+                            show_difficulty_menu = False
+                            print(f"Selected difficulty: {selected_difficulty}")
+
         screen.fill(white)
 
-        button_widht, button_height = 300, 80
-        button_x = screen_width // 2 - button_widht // 2
+        button_width, button_height = 300, 80
+        button_x = screen_width // 2 - button_width // 2
         button_y_start = screen_height // 2 - 100
         button_gap = 20
 
@@ -58,17 +92,21 @@ def general_quiz_app():
 
         for i, button_text in enumerate(buttons):
             button_y = button_y_start + i * (button_height + button_gap)
-            if button_x < mouse_pos[0] < button_x + button_widht and button_y < mouse_pos[1] < button_y + button_height:
+            if button_x < mouse_pos[0] < button_x + button_width and button_y < mouse_pos[1] < button_y + button_height:
                 button_color = dark_grey
             else:
                 button_color = grey
 
-            pygame.draw.rect(screen, button_color, (button_x, button_y, button_widht, button_height))
-            pygame.draw.rect(screen, black, (button_x, button_y, button_widht, button_height), 2)
+            pygame.draw.rect(screen, button_color, (button_x, button_y, button_width, button_height))
+            pygame.draw.rect(screen, black, (button_x, button_y, button_width, button_height), 2)
             button_text_rendered = font_button.render(button_text, True, black)
-            screen.blit(button_text_rendered, (button_x + button_widht // 2 - button_text_rendered.get_width() // 2,
+            screen.blit(button_text_rendered, (button_x + button_width // 2 - button_text_rendered.get_width() // 2,
                                                button_y + button_height // 2 - button_text_rendered.get_height() // 2))
-        
+
+        if show_difficulty_menu:
+            draw_difficulty_menu(mouse_pos)
+
         pygame.display.flip()
+
     pygame.quit()
     sys.exit()
